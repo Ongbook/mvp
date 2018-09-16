781f9fee112e92714fff9a76a5d85b9e22da4b45
Eduardo Moura <edoura@gmail.com>
2018-09-16 19:51:10 -0300

    up layout modal cadastro

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import {  BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 import { BuscaCnpjService } from '../busca-cnpj.service';
@@ -12,7 +12,9 @@ import { BuscaLatLngService } from '../busca-lat-lng.service';
 })
 export class HomeComponent implements OnInit {
 	navbarOpen = false;
-	
+
+	areaAtuacaoPrincipal: string;
+	razaoSocial: string;
 	fantasia: string;
 	email: string;
 	result: boolean = false;
@@ -20,51 +22,53 @@ export class HomeComponent implements OnInit {
 	lat: number = -19.82519036;
 	lng: number = -40.65804826;
 
-	public modalRef: BsModalRef; 
+	public modalRef: BsModalRef;
 
-	constructor(private modalService: BsModalService, private BuscaCnpjService: BuscaCnpjService, private BuscaLatLngService: BuscaLatLngService) {}
+	constructor(private modalService: BsModalService, private BuscaCnpjService: BuscaCnpjService, private BuscaLatLngService: BuscaLatLngService) { }
 
 	ngOnInit() {
 	}
 
 	public openModal(template: TemplateRef<any>) {
-		this.modalRef = this.modalService.show(template); 
+		this.modalRef = this.modalService.show(template);
 	}
 
-	findCnpj(value){
+	findCnpj(value) {
 
 		this.BuscaCnpjService.getCnpj(value).subscribe((res) => {
 
 			console.log(res);
 
-		//concat
-		let endereco = res['logradouro'] + ', '+ res['numero'] +' - '+res['bairro'] + ', ' + res['municipio'] + '-' + res['uf'];
+			//concat
+			let endereco = res['logradouro'] + ', ' + res['numero'] + ' - ' + res['bairro'] + ', ' + res['municipio'] + '-' + res['uf'];
 
-		console.log(endereco);
+			console.log(endereco);
 
-		this.result = true;
+			this.result = true;
 
-		this.fantasia = res['fantasia'];
-		this.email = res['email'];
-
-
-		this.BuscaLatLngService.getlatlng(endereco).subscribe(data => {
-
-			let result = data['results'];
+			this.areaAtuacaoPrincipal = res['atividade_principal'][0].text;
+			this.razaoSocial = res['nome'];
+			this.fantasia = res['fantasia'];
+			this.email = res['email'];
 
 
-			this.lat = result[0].geometry.location.lat;
-			this.lng = result[0].geometry.location.lng;
+			this.BuscaLatLngService.getlatlng(endereco).subscribe(data => {
 
-			this.showMap = true;
-			
-		}, errr => {
-			console.log(errr);
+				let result = data['results'];
+
+
+				this.lat = result[0].geometry.location.lat;
+				this.lng = result[0].geometry.location.lng;
+
+				this.showMap = true;
+
+			}, errr => {
+				console.log(errr);
+			});
+
+		}, err => {
+			console.log(err);
 		});
-
-	}, err => {
-		console.log(err);
-	});
 	}
 
 	toggleNavbar() {
